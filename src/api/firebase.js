2187edbf2124dc61dac965ff8d9dd8547688295c
jaimemin <jaimemin@gmail.com>
2023-04-22 23:38:01 +0900

    initial commit

---
+++ src/api/firebase.js
@@ -0,0 +1,70 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { getDatabase, ref, set, get } from "firebase/database";
+import { v4 as uuid } from "uuid";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+const database = getDatabase(app);
+
+export function login() {
+  signInWithPopup(auth, provider).catch((error) => {
+    console.error(error);
+  });
+}
+
+export function logout() {
+  signOut(auth).then(() => null);
+}
+
+export function onUserStateChange(callback) {
+  onAuthStateChanged(auth, async (user) => {
+    // 1. 사용자가 로그인한 경우
+    const updatedUser = user ? await adminUser(user) : null;
+
+    callback(updatedUser);
+  });
+}
+
+async function adminUser(user) {
+  // 2. 사용자가 어드민 권한을 가지고 있는지 확인!
+  // 3. {...user, isAdmin: true/false}
+  return get(ref(database, "admins")).then((snapshot) => {
+    if (snapshot.exists()) {
+      const admins = snapshot.val();
+      const isAdmin = admins.includes(user.uid);
+      // const isAdmin = false;
+
+      return { ...user, isAdmin };
+    }
+
+    return user;
+  });
+}
+
+export async function addNewProduct(product, imageUrl) {
+  const id = uuid();
+
+  return set(ref(database, `products/${id}`), {
+    ...product,
+    id,
+    price: parseInt(product.price),
+    image: imageUrl,
+    options: product.options.split(","),
+  });
+}
